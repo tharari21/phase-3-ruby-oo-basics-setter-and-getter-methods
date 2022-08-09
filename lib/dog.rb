3a99365df1ef4955823010d74d8426986966ddab
Tomer Harari <tharari21@gmail.com>
2022-08-09 18:48:52 -0400

    commit tree 6776e575e9ee54905d1d150c0be88b12a5c78137

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,14 @@
 class Dog
-
+	def name=(value)
+		@name = value
+	end
+	def name
+		@name
+	end
+	def breed=(value)
+		@breed = value
+	end
+	def breed
+		@breed
+	end
 end
--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,20 @@
 class Person
-
+	def name=(value)
+		@name = value
+	end
+	def name
+		@name
+	end
+	def job=(value)
+		@job = value
+	end
+	def job
+		@job
+	end
 end
+
+p = Person.new
+p.name = 'Tomer'
+p.job = 'coder'
+puts p.name
+puts p.job
